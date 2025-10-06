8c9a99d11ddf81fa7222317a7bf3442fbc167acd
Luciano123AB <Lucianoedustefa24032004@gmail.com>
2025-10-06 00:44:32 -0300

    Update Boot.php

--- app/Services/Boot.php
+++ app/Services/Boot.php
@@ -6,16 +6,20 @@ use Illuminate\Support\Facades\Artisan;
 
 class Boot
 {
-    public static function comandos() {
-        Artisan::call("", [
+    public static function criarPovoarBanco() {
+        Artisan::call("migrate", [
             "--force" => true
         ]);
-        Artisan::call("", [
-            "--class" => "",
+        Artisan::call("db:seed", [
+            "--class" => "UsuariosTableSeeder",
             "--force" => true
         ]);
-        shell_exec("npm install 2>&1");
 
         session(["boot" => true]);
+    }
+
+    public static function dependencias() {
+        shell_exec("composer install 2>&1");
+        shell_exec("npm install 2>&1");
     }
 }
