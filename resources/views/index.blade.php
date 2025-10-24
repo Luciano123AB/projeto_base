4585cf758b3e99596a55a0fc7cd2c56a05d89c6e
Luciano Eduardo <luciano123ab@gmail.com>
2025-10-23 21:16:02 -0300

    master

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,11 @@
 @extends("layouts.main_layout")
 
 @section("content")
-    <h1 class="fs-bold">Ola Mundo!</h1>
+    <div class="position-absolute top-50 start-50 translate-middle">
+        <h1 class="fs-bold">Ola Mundo!</h1>
 
-    <p class="my-5">...</p>
+        <p class="my-5">...</p>
 
-    <footer><img style="width: 35px; height: 35px;" class="border border-black" src="{{ asset("assets/images/perfil.png") }}"> Todos os Direitos Reservados: Luciano Eduardo Stefanello da Silva</footer>
+        <footer><img style="width: 35px; height: 35px;" class="border border-black" src="{{ asset("assets/images/perfil.png") }}"> Todos os Direitos Reservados: Luciano Eduardo Stefanello da Silva</footer>
+    </div>
 @endsection
